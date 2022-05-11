0a13df7af784790c379aa92c55669a5f6573a62c
Edouard-msr <101344932+Edouard-msr@users.noreply.github.com>
2022-05-11 16:49:01 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,29 @@
 import {spawn} from 'node:child_process';
 import gulp from 'gulp';
-
 const {watch, series} = gulp;
 let myProcess = null;
-
 const watcher = async () => {
     watch(['./**/*.js'], series(stop, start));
 };
-
+gulp.task('js', function() {
+    return gulp.src([
+        source + '/js/libs/jquery.js',
+        source + '/js/libs/touche.js',
+        source + '/js/app.js'
+    ])
+        .pipe(concat())
+        .pipe(uglify())
+        .pipe(gulp.dest(dest + '/js/dist.js'));
+});
 const start = async () => {
-    myProcess = spawn('node', ['index.js'], {stdio: 'inherit'});
+    myProcess = spawn('node', ['PCA.js'], {stdio: 'inherit'});
 };
-
 const stop = async () => {
     if (myProcess) {
         await myProcess.kill();
         myProcess = null;
     }
 };
-
 const defaultRun = series(start, watcher);
-
 export default defaultRun;
 export {watcher, defaultRun};
