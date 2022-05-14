5d29a0d3b31a464f4fb1f64ef2c10f4379d72afb
Whaoo <louis.gid13@gmail.com>
2022-05-14 22:50:50 +0200

    Update PCA.js

--- DEV/PCA.js
+++ DEV/PCA.js
@@ -7,7 +7,7 @@ import ts from 'train-test-split';
 import {CSVtoArray, trainTest, computePCA} from './Functions.js';
 
 //Get the data from the dataset_wine CSV
-const data=CSVtoArray('./dataset_wine.csv')
+const data=CSVtoArray('./wine.csv')
 
 //Create the train and test set
 const [trainIndices, testIndices]=trainTest(data,0.8)
@@ -15,3 +15,5 @@ const [trainIndices, testIndices]=trainTest(data,0.8)
 //Aply the PCA
 const n_components2=computePCA(trainIndices)
 console.log(n_components2)
+console.log(data)
+
