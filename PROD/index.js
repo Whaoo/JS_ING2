65dae2342f4011c6d5ec029fb6e11f48fe1f6b07
Whaoo <louis.gid13@gmail.com>
2022-05-15 19:09:02 +0200

    Delete PROD directory

--- PROD/index.js
+++
@@ -1,8 +0,0 @@
-import {n_components2} from './PCA.js';
-import {out} from './Chi2.js'
-
-console.log('PCA de wine.csv');
-console.log(n_components2);
-
-console.log('Khi2/Chi2 de zoo.csv');
-console.log(out);
