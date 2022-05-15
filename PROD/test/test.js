54f8a36d6e0038a440fb0e958c16876b44916779
Whaoo <louis.gid13@gmail.com>
2022-05-15 19:45:12 +0200

    Update test.js

--- PROD/test/test.js
+++ PROD/test/test.js
@@ -3,7 +3,7 @@ import assert from 'node:assert';
 describe('Array', () => {
 	describe('#indexOf()', () => {
 		it('should return done when the value is not present', () => {
-			assert.equal([1, 2, 3].indexOf('4'), 1);
+			assert.equal([1, 2, 3].indexOf('4'), -1);
 		});
 	});
 });
